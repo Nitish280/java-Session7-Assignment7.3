46ef3872ad08e4b792ca0794560f8391c0fa14f0
Nitish280 <nsrccs@gmail.com>
2017-06-13 14:09:19 +0530

    Create Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,21 +1,22 @@
 
 public class Student{
-	
+	//here we create the first method
 	public int add(int x, int y) {
 		System.out.println("add with two args called");
 		return x +y;
 	}
+	//here we create the second method
 	public int add(int x, int y, int z) {
 		System.out.println("add with three args called");
+		//here we call the 1st method from the second method
 		this.add(x,y);
 		return x + y + z;
 
 	}
 
 	public static void main(String[] args) {
+		//here we creating the new object
 		Student st = new Student();
-		
-		
 		System.out.println(st.add(5,8));
 		System.out.println(st.add(5,8,6));
 	
